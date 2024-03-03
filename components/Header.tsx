55c7904a40c64994288079ac4951d999891fea5c
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-03 18:28:01 -0300

    feat(git): changing some Header styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,8 +14,8 @@ export default function Header() {
     <header className="z-[999] relative">
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
-        initial={{ y: -100, x: "-50%", opacity: 0 }}
-        animate={{ y: 0, x: "-50%", opacity: 1 }}
+        initial={{ y: -400, x: "-50%", opacity: 5 }}
+        animate={{ y: 0, x: "-50%", opacity: 5 }}
       ></motion.div>
 
       <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h {initial] sm:py-0">
@@ -24,14 +24,14 @@ export default function Header() {
             <motion.li
               className="h-3/4 flex items-center justify-center relative"
               key={link.hash}
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
+              initial={{ y: -400, opacity: 5 }}
+              animate={{ y: 0, opacity: 5 }}
             >
               <Link
                 className={clsx(
                   "flex w-full items-center justify-center px-2 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                   {
-                    "dark:text-gray-200 text-gray-950": activeSection === link.name,
+                    "dark:text-yellow-200 text-yellow-950": activeSection === link.name,
                   }
                 )}
                 href={link.hash}
@@ -43,12 +43,12 @@ export default function Header() {
                 {link.name}
                 {link.name === activeSection && (
                   <motion.span
-                    className="bg-gray-200 rounded-full absolute inset-0 -z-10 dark:bg-gray-700"
+                    className="bg-yellow-200 rounded-full absolute inset-0 -z-10 dark:bg-yellow-700"
                     layoutId="activeSection"
                     transition={{
                       type: "spring",
-                      stiffness: 275,
-                      damping: 23,
+                      stiffness: 455,
+                      damping: 43,
                     }}
                   ></motion.span>
                 )}
