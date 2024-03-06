f1795cc1d0fcfac28d512ee430a90fa406ffaa23
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-06 17:38:14 -0300

    feat(git): creating over component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import About from "@/components/About";
 import { Hero } from "@/components/Hero";
+import SectionDivider from "@/components/SectionDivider";
 import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-left px-4">
+    <main className="flex flex-col items-center px-4">
       <Hero />
       <Skills />
+      <SectionDivider />
+      <About />
     </main>
   );
 }
--- src/components/SectionHeading/index.tsx
+++ src/components/SectionHeading/index.tsx
@@ -6,6 +6,6 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({
   children }: SectionHeadingProps) {
-  return <h1 className="text-3xl font-bold text-purple-500 capitalize mb-8 text-center"
+  return <h1 className="text-3xl underline font-bold text-purple-500 capitalize mb-8 text-center"
   >{children}</h1>;
 }
