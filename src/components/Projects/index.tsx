156a24b845c810c149ff5fb9fc12d15a68ed8115
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-17 02:34:39 -0300

    feat(git): adding projects to the projects component

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -13,30 +13,65 @@ import { motion } from "framer-motion";
 
 const projectList: IProjectCardProps[] = [
   {
+    name: "AluraCases",
+    description: "Explorei o Framework Next.js na Alura, aprendendo sobre criação de projetos, geração de conteúdo e SEO. Pronto para aplicar esses conhecimentos!",
+    thumb: "/cases.png",
+    technologies: ['Next.js', 'GIT', 'TypeScipt'],
+    link: 'https://alura-cases-theta.vercel.app/'
+  },
+  {
     name: "ByteBank",
     description: "Sistema de gerenciamento bancário, com TypeScript para a interface amigável, Git para controle de versão.",
     thumb: "/image.png",
-    technologies: ['GIT', 'TypeScipt'],
-    link: 'https://bytebank2.vercel.app/'
+    technologies: ['TypeScipt', 'GIT'],
+    link: 'https://bytebank2-p6v90fcjb-kayoennrique.vercel.app/'
   },
   {
-    name: "Negotiation",
-    description: "Projeto de Gestão de Tarefas: TypeScript para interface intuitiva, Git para versionamento.",
-    thumb: "/image2.png",
-    technologies: ['GIT', 'TypeScipt'],
-    link: 'https://negotiation-six.vercel.app/'
+    name: "Hello World!",
+    description: "Projeto em React Router com JavaScript, implementando rotas, páginas de erro 404 e rotas dinâmicas",
+    thumb: "/hello-world.png",
+    technologies: ['React', 'JavaScript', 'GIT',],
+    link: 'https://helloworld-green-one.vercel.app/'
   },
   {
-    name: "Devs &  Dragons",
-    description:
-      "Projeto pessoal: cartas em batalha usando JavaScript para lógica, CSS para design e Git para controle. Uma experiência envolvente e estratégica.",
-    thumb: "/image3.png",
-    technologies: ['JavaScript', 'GIT', 'CSS'],
-    link: 'https://devsedragons.vercel.app/'
+    name: "Organo",
+    description: "Criado em React, adaptei Organo 2.0 com melhorias, aprendendo sobre props, closures, e estados. Refatorei e compreendi os fundamentos do React.",
+    thumb: "/organo.png",
+    technologies: ['React', 'GIT', 'TypeScript'],
+    link: 'https://organo-alpha-five.vercel.app/'
+  },
+  {
+    name: "Studies",
+    description: "Desenvolvi em React com TypeScript, SASS, componentes de classe e função, manipulação de props e arrays",
+    thumb: "/studies.png",
+    technologies: ['React', 'TypeScript', 'GIT', 'SASS'],
+    link: 'https://studies-dun.vercel.app/'
+  },
+  {
+    name: "AlfaBit",
+    description: "Explorei Next.js e Tailwind, abordando setup inicial, tokens de design, Atomic Design, Headless UI, Storybook e publicação de bibliotecas de componentes.",
+    thumb: "/alfabit.png",
+    technologies: ['Next.js', 'Tailwind', 'NPM', 'Storybook'],
+    link: 'https://alfabit.vercel.app/?path=/story/design-system-molecules-notice--primary'
+  },
+  {
+    name: "Cinetag",
+    description: "Projeto feito para prática do uso do React",
+    thumb: "/cinetag.png",
+    technologies: ['React', 'CSS Modules', 'GIT'],
+    link: 'https://cinetag-two-omega.vercel.app/'
+  },
+  {
+    name: "Meteora",
+    description: "React gerenciando o estado globalmente com ContextAPI",
+    thumb: "/meteora.png",
+    technologies: ['React', 'ContextAPI', 'GIT'],
+    link: 'https://meteora-bice-rho.vercel.app/'
   }
 ]
 
 export const Projects = () => {
+
   const { ref } = useSectionInView("Projetos", 0.5);
 
   return (
