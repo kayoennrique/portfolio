413114a598c504cec25c72e50dad07280578c6d1
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-20 21:56:38 -0300

    feat(git): testing translation

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -12,91 +12,92 @@ import { motion } from "framer-motion";
 import { useTranslations } from "next-intl";
 
 
-const projectList: IProjectCardProps[] = [
-  {
-    name: "AluraCases",
-    description: "Explorei o Framework Next.js na Alura, aprendendo sobre criação de projetos, geração de conteúdo e SEO. Pronto para aplicar esses conhecimentos!",
-    thumb: "/cases.png",
-    technologies: ['Next.js', 'GIT', 'TypeScipt'],
-    link: 'https://alura-cases-theta.vercel.app/'
-  },
-  {
-    name: "ByteBank",
-    description: "Sistema de gerenciamento bancário, com TypeScript para a interface amigável, Git para controle de versão.",
-    thumb: "/image.png",
-    technologies: ['TypeScipt', 'GIT'],
-    link: 'https://bytebank2-p6v90fcjb-kayoennrique.vercel.app/'
-  },
-  {
-    name: "Hello World!",
-    description: "Projeto em React Router com JavaScript, implementando rotas, páginas de erro 404 e rotas dinâmicas",
-    thumb: "/hello-world.png",
-    technologies: ['React', 'JavaScript', 'GIT',],
-    link: 'https://helloworld-green-one.vercel.app/'
-  },
-  {
-    name: "Organo",
-    description: "Criado em React, adaptei Organo 2.0 com melhorias, aprendendo sobre props, closures, e estados. Refatorei e compreendi os fundamentos do React.",
-    thumb: "/organo.png",
-    technologies: ['React', 'GIT', 'TypeScript'],
-    link: 'https://organo-alpha-five.vercel.app/'
-  },
-  {
-    name: "Studies",
-    description: "Desenvolvi em React com TypeScript, SASS, componentes de classe e função, manipulação de props e arrays",
-    thumb: "/studies.png",
-    technologies: ['React', 'TypeScript', 'GIT', 'SASS'],
-    link: 'https://studies-dun.vercel.app/'
-  },
-  {
-    name: "AlfaBit",
-    description: "Explorei Next.js e Tailwind, abordando setup inicial, tokens de design, Atomic Design, Headless UI, Storybook e publicação de bibliotecas de componentes.",
-    thumb: "/alfabit.png",
-    technologies: ['Next.js', 'Tailwind', 'NPM', 'Storybook'],
-    link: 'https://alfabit.vercel.app/?path=/story/design-system-molecules-notice--primary'
-  },
-  {
-    name: "Cinetag",
-    description: "Projeto feito para prática do uso do React",
-    thumb: "/cinetag.png",
-    technologies: ['React', 'CSS Modules', 'GIT'],
-    link: 'https://cinetag-two-omega.vercel.app/'
-  },
-  {
-    name: "Meteora",
-    description: "React gerenciando o estado globalmente com ContextAPI",
-    thumb: "/meteora.png",
-    technologies: ['React', 'ContextAPI', 'GIT'],
-    link: 'https://meteora-bice-rho.vercel.app/'
-  },
-  {
-    name: "Aluroni",
-    description: "Criei um projeto React do zero usando Create React App, adotando melhores práticas de CSS, imports absolutos, SVGs com SVGR e 'classnames'.",
-    thumb: "/aluroni.png",
-    technologies: ['React', 'GIT', 'SASS', 'NPM'],
-    link: 'https://aluroni-5ujg.vercel.app/'
-  },
-  {
-    name: "ByteBooks",
-    description: "React com Server Side Rendering para otimizar desempenho na aplicação. Uso de useContext, useMemo, e manipulação de estado global no carrinho.",
-    thumb: "/bytebooks.png",
-    technologies: ['React', 'SSR', 'GIT', 'Hooks'],
-    link: 'https://alurabooks-init-1h98a27r6-kayoennrique.vercel.app/'
-  },
-  {
-    name: "VidFlow",
-    description: "JavaScript, consumindo e processando dados de uma API para criar experiências web dinâmicas e interativas.",
-    thumb: "/vidflow.png",
-    technologies: ['JavaScript', 'GIT', 'API', 'NPM'],
-    link: 'https://vidflow-4d9n.vercel.app/'
-  },
-]
 
 export const Projects = () => {
 
   const { ref } = useSectionInView("Projetos", 0.5);
 
   const t = useTranslations("Projects");
+
+  const projectList: IProjectCardProps[] = [
+    {
+      name: "AluraCases",
+      description: `${t("AluraCases.description")}`,
+      thumb: "/cases.png",
+      technologies: ['Next.js', 'GIT', 'TypeScipt'],
+      link: 'https://alura-cases-theta.vercel.app/'
+    },
+    {
+      name: "ByteBank",
+      description: "Sistema de gerenciamento bancário, com TypeScript para a interface amigável, Git para controle de versão.",
+      thumb: "/image.png",
+      technologies: ['TypeScipt', 'GIT'],
+      link: 'https://bytebank2-p6v90fcjb-kayoennrique.vercel.app/'
+    },
+    {
+      name: "Hello World!",
+      description: "Projeto em React Router com JavaScript, implementando rotas, páginas de erro 404 e rotas dinâmicas",
+      thumb: "/hello-world.png",
+      technologies: ['React', 'JavaScript', 'GIT',],
+      link: 'https://helloworld-green-one.vercel.app/'
+    },
+    {
+      name: "Organo",
+      description: "Criado em React, adaptei Organo 2.0 com melhorias, aprendendo sobre props, closures, e estados. Refatorei e compreendi os fundamentos do React.",
+      thumb: "/organo.png",
+      technologies: ['React', 'GIT', 'TypeScript'],
+      link: 'https://organo-alpha-five.vercel.app/'
+    },
+    {
+      name: "Studies",
+      description: "Desenvolvi em React com TypeScript, SASS, componentes de classe e função, manipulação de props e arrays",
+      thumb: "/studies.png",
+      technologies: ['React', 'TypeScript', 'GIT', 'SASS'],
+      link: 'https://studies-dun.vercel.app/'
+    },
+    {
+      name: "AlfaBit",
+      description: "Explorei Next.js e Tailwind, abordando setup inicial, tokens de design, Atomic Design, Headless UI, Storybook e publicação de bibliotecas de componentes.",
+      thumb: "/alfabit.png",
+      technologies: ['Next.js', 'Tailwind', 'NPM', 'Storybook'],
+      link: 'https://alfabit.vercel.app/?path=/story/design-system-molecules-notice--primary'
+    },
+    {
+      name: "Cinetag",
+      description: "Projeto feito para prática do uso do React",
+      thumb: "/cinetag.png",
+      technologies: ['React', 'CSS Modules', 'GIT'],
+      link: 'https://cinetag-two-omega.vercel.app/'
+    },
+    {
+      name: "Meteora",
+      description: "React gerenciando o estado globalmente com ContextAPI",
+      thumb: "/meteora.png",
+      technologies: ['React', 'ContextAPI', 'GIT'],
+      link: 'https://meteora-bice-rho.vercel.app/'
+    },
+    {
+      name: "Aluroni",
+      description: "Criei um projeto React do zero usando Create React App, adotando melhores práticas de CSS, imports absolutos, SVGs com SVGR e 'classnames'.",
+      thumb: "/aluroni.png",
+      technologies: ['React', 'GIT', 'SASS', 'NPM'],
+      link: 'https://aluroni-5ujg.vercel.app/'
+    },
+    {
+      name: "ByteBooks",
+      description: "React com Server Side Rendering para otimizar desempenho na aplicação. Uso de useContext, useMemo, e manipulação de estado global no carrinho.",
+      thumb: "/bytebooks.png",
+      technologies: ['React', 'SSR', 'GIT', 'Hooks'],
+      link: 'https://alurabooks-init-1h98a27r6-kayoennrique.vercel.app/'
+    },
+    {
+      name: "VidFlow",
+      description: "JavaScript, consumindo e processando dados de uma API para criar experiências web dinâmicas e interativas.",
+      thumb: "/vidflow.png",
+      technologies: ['JavaScript', 'GIT', 'API', 'NPM'],
+      link: 'https://vidflow-4d9n.vercel.app/'
+    },
+  ]
 
   return (
     <div className="overflow-hidden w-full ">
