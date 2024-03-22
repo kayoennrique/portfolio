a0b50140435f82128d76c35c2b326bfb7e4ca4e3
Kayo Ennrique <138163354+kayoennrique@users.noreply.github.com>
2024-03-22 04:18:20 -0300

    feat(git): Update ToggleLanaguage

--- src/components/ToggleLanguage/index.tsx
+++ src/components/ToggleLanguage/index.tsx
@@ -20,7 +20,7 @@ export function LanguageToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className='fixed bottom-5 my-16 left-5 bg-gray-400 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 rounded-full shadow-2xl flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950'>
+        <button className='fixed bottom-5 my-16 left-5 bg-gray-400 w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 rounded-full shadow-2xl flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950 z-10'>
           <Languages className="h-[1.2rem] w-[1.2rem]" />
           <span className="sr-only">Toggle Language</span>
         </button>
